96315b5749168fa08f01f570eb0919bf7ef0c2fc
艺理同奇 <yilitongqi@gmail.com>
2025-11-27 21:10:40 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -41,14 +41,14 @@ module.exports = {
       artist: 'Falcom Sound Team jdk',
       url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
       cover:
-        'https://raw.githubusercontent.com/yilitongqi/tuchuang/refs/heads/main/%E9%9F%B3%E4%B9%90%E5%9B%BE%E6%A0%87.jpg'
+        'https://raw.githubusercontent.com/yilitongqi/tuchuang/refs/heads/main/%E5%8F%A4%E5%AD%97%E7%82%B9-%E9%BB%91%E5%BA%951_%E5%89%AF%E6%9C%AC.png'
     },
     {
       name: '王都グランセル',
       artist: 'Falcom Sound Team jdk',
       url: 'https://music.163.com/song/media/outer/url?id=731355.mp3',
       cover:
-        'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+        'https://raw.githubusercontent.com/yilitongqi/tuchuang/refs/heads/main/%E5%8F%A4%E5%AD%97%E7%82%B9-%E9%BB%91%E5%BA%951_%E5%89%AF%E6%9C%AC.png'
     }
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
