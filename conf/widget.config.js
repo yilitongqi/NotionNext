4d9b205c76dc36700403b04b4f825977025b8912
艺理同奇 <yilitongqi@gmail.com>
2025-11-27 20:24:40 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -41,7 +41,7 @@ module.exports = {
       artist: 'Falcom Sound Team jdk',
       url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
       cover:
-        'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+        'https://raw.githubusercontent.com/yilitongqi/tuchuang/refs/heads/main/%E9%9F%B3%E4%B9%90.svg'
     },
     {
       name: '王都グランセル',
